77dfd08842903a01928caf0bf01831c8226e6bc7
daijiromaeyama <daijiro@opengate07-221170.sso.cc.saga-u.ac.jp>
2017-10-29 18:44:02 +0900

    use select box for article status

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
   enum status: { draft: 0, published: 1 }
-  validates :title, :content, :status, presence: true
+  validates :status, inclusion: { in: Article.statuses.keys }
 end
